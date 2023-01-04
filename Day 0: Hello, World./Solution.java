30bf09cd37f198c40633ed1bac4143a91e85c428
Harsha T R <63777708+TRHarsha@users.noreply.github.com>
2023-01-04 22:27:56 +0530

    Hello World
    To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, and finally, print the value of your variable on a second line.

---
+++ Day 0: Hello, World./Solution.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String inputString = scan.nextLine();
+        System.out.println("Hello, World.");
+        System.out.println(inputString);
+    }
+}
