18a7b2e57793227f3a825563110a37f6b37af220
Harsha T R <63777708+TRHarsha@users.noreply.github.com>
2023-01-07 20:16:25 +0530

    Multiplication Table
    Given an integer, n, print its first 10 multiples. Each multiple n xi (where
    1 ≤ i ≤10) should be printed on a new line in the form: n x i = result.

---
+++ Day 5: Loops/Solution.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args){
+int num,i;
+Scanner sc =new Scanner(System.in);
+num= sc.nextInt();
+for (i=1;i<=10;i++)
+System.out.println(num+" x "+i+" = "+num*i);
+    }
+}
